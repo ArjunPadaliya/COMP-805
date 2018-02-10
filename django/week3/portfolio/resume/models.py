ef76136efda1d7aa22a3e3122b55d0054df869b1
ArjunPadaliya <ap1170@wildcats.unh.edu>
2018-02-09 21:34:52 -0500

    created models experience and education

--- django/week3/portfolio/resume/models.py
+++ django/week3/portfolio/resume/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from datetime import date
 # Create your models here.
+
+class Experience(models.Model):
+    title=models.CharField(max_length=64, null=True, blank=False)
+    location=models.CharField(max_length=64, null=True, blank=False)
+    start_date=models.DateField(blank=True, null=True)
+    end_date=models.DateField(blank=True, null=True)
+    description=models.CharField(max_length=256, null=False, blank= True)
+
+class Education(models.Model):
+    institution_name=models.CharField(max_length=256, null=True, blank=False)
+    location=models.CharField(max_length=64, null=True, blank=False)
+    degree=models.CharField(max_length=64, null=True, blank=False)
+    major=models.CharField(max_length=64, null=True, blank=False)
+    gpa=models.FloatField(null=True, blank=True)
