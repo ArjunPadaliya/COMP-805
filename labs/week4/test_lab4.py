fe8d0a27f50962f6b39b82c4c3fb75d45f2f43bb
ArjunPadaliya <ap1170@wildcats.unh.edu>
2018-02-20 17:09:39 -0500

    added docstrings in test_lab4 file

--- labs/week4/test_lab4.py
+++ labs/week4/test_lab4.py
@@ -1,7 +1,14 @@
+"""
+test_lab4.py
+Arjun Padaliya
+1/30/2018
+"""
+
 import unittest
 class Lab4Test(unittest.TestCase):
     def test_squared(self):
         """
+        testing squared function
         """
         func=lab4.squared
         case1=[1, 2, 3]
@@ -10,6 +17,7 @@ class Lab4Test(unittest.TestCase):
 
     def test_check_title(self):
         """
+        testing check_title function
         """
         func=lab4.check_title
         case2=["Hello World", "hi", "Hello"]
@@ -18,6 +26,7 @@ class Lab4Test(unittest.TestCase):
 
     def test_restock_inventory(self):
         """
+        testing restock_inventory function
         """
         func=lab4.restock_inventory
         case3={'a':10, 'b':5, 'c':7}
@@ -26,6 +35,7 @@ class Lab4Test(unittest.TestCase):
 
     def test_filter_0_items(self):
         """
+        testing filter_0_items function
         """
         func=lab4.filter_0_items
         case4={'a':10, 'b':0, 'c':7}
@@ -34,6 +44,7 @@ class Lab4Test(unittest.TestCase):
 
     def test_average_grades(self):
         """
+        testing average_grades function
         """
         func=lab4.average_grades
         case5={'Michael' :[100, 78, 88, 900/10], 'Daniel':[80, 95, 77, 64.0], 'Josh':[99, 89, 94, 66]}
