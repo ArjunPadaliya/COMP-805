b786330e34462ad80726b5be19d7c7298c943be0
ArjunPadaliya <ap1170@wildcats.unh.edu>
2018-02-20 10:24:58 -0500

    updated views with minor changes

--- django/week4/portfolio/resume/views.py
+++ django/week4/portfolio/resume/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
-from .models import Education, Experience
+from .models import Resume, Education, Experience
 # Create your views here.
 
 def home(request):
     '''
     Renders home page from resume templates
     '''
-    #qsEx=Experience.objects.all()
-    #qsEdu=Education.objects.all()
-    qsResume=Resume.objects.all()
-    context={'experience': qsEx, 'education':qsEdu, 'resume':qsResume, 'res':'active'}
+    qsEx=Experience.objects.all()
+    qsEdu=Education.objects.all()
+    qsResume=Resume.objects.first()
+    context={'education':qsEdu, 'experience':qsEx, 'resume':qsResume, 'res':'active'}
     return render(request, 'resume/home.html', context)
