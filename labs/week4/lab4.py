63913f85a034a67f456d3fa73eacef4eed167759
ArjunPadaliya <ap1170@wildcats.unh.edu>
2018-02-19 23:26:49 -0500

    added lab4 with unittest

--- labs/week4/lab4.py
+++ labs/week4/lab4.py
@@ -3,75 +3,69 @@ lab4.py
 Arjun Padaliya
 1/30/2018
 """
-import unittest
-class Lab4Test(unittest.TestCase):
-	def test_squared(self):
-		"""
-		Tests lab4.squared()
-		"""
-		func=lab4.squared
-		case1=[1,2,3]
-		expected=[1, 4, 9]
-		self.assertEqual(func(case1), expected1)
 
-	def check_title(title_list):
-		"""
-		Removes strings in title_list that have numbers and aren't title case
-		title_list: list of strings
-		Returns: list of strings that are titles
-		"""
-		for item in title_list:
-			if not item.istitle():
-				title_list.remove(item)
-		return title_list
+def squared(num_list):
+	"""
+	Squares numbers in num_list
+	num_list:list of numbers
+	Returns:list of these numbers squared
+	"""
+	new_list=[]
+	for num in num_list:
+		sq_num=pow(num, 2)
+		new_list.append(sq_num)
+	return new_list
 
-
-	def restock_inventory(inventory):
-		"""
-		Increases inventory of each item in dictionary by 10
-		inventory: a dictionary with:
-		key: string that is the name of the inventory item
-		value: integer that equals the number of that item currently on had
-		Returns: updated dictionary where each inventory item is restocked
-		"""
-		for item in inventory:
-			inventory[item]+=10
-
-		return inventory
+def check_title(title_list):
+	"""
+	Removes strings in title_list that have numbers and aren't title case
+	title_list: list of strings
+	Returns: list of strings that are titles
+	"""
+	for item in title_list:
+		if not item.istitle():
+			title_list.remove(item)
+	return title_list
 
 
-	def filter_0_items(inventory):
-		"""
-		Removes items that have a value of 0 from a dictionary of inventories
-		inventory: a dictionary with:
-		key: string that is the name of the inventory item
-		value: integer that equals the number of that item currently on had
-		Returns: the same inventory_dict with any item had 0 quantity removed
-		"""
-		new_list=[]
+def restock_inventory(inventory):
+	"""
+	Increases inventory of each item in dictionary by 10
+	inventory: a dictionary with:
+	key: string that is the name of the inventory item
+	value: integer that equals the number of that item currently on had
+	Returns: updated dictionary where each inventory item is restocked
+	"""
+	new_inventory={}
+	for item in inventory:
+		new_inventory[item]=inventory[item]+10
 
-		for item in inventory:
-			if inventory[item]==0:
-				new_list.append(item)
+	return new_inventory
 
-		for keys in new_list:
-			del inventory[keys]
 
-		return inventory
+def filter_0_items(inventory):
+	"""
+	Removes items that have a value of 0 from a dictionary of inventories
+	inventory: a dictionary with:
+	key: string that is the name of the inventory item
+	value: integer that equals the number of that item currently on had
+	Returns: the same inventory_dict with any item had 0 quantity removed
+	"""
+	for k in inventory.copy():
+		if inventory[k] == 0:
+			inventory.pop(k)
+	return inventory
 
-	def average_grades(grades):
-		"""
-		Takes grade values from a dictionary and averages them into a final grade
-		grades: a dictionary of grades with:
-		key: string of students name
-		value: list of integer grades received in class
-		Returns: dictionary that averages out the grades of each student
-		"""
-		for student in grades: #get grades for every individual student
-			total_marks=0 #initialize total_marks
-			for marks in grades[student]: #iterate over list of marks of each student
-				total_marks+=marks #sum up all the marks
-			#update value of that student in dictionary (grades) by dividing total marks with available number of marks in list
-			grades[student]=total_marks/len(grades[student])
-
-		return grades
+def average_grades(grades):
+	"""
+	Takes grade values from a dictionary and averages them into a final grade
+	grades: a dictionary of grades with:
+	key: string of students name
+	value: list of integer grades received in class
+	Returns: dictionary that averages out the grades of each student
+	"""
+	avg_grades={}
+	for student_grade in grades: #get grades for every individual student
+		#print (grades[student_grade])
+		avg_grades[student_grade]=sum(grades[student_grade])/len(grades[student_grade])
+	return avg_grades
--- labs/week4/test_lab4.py
+++ labs/week4/test_lab4.py
@@ -1,4 +1,47 @@
 import unittest
+class Lab4Test(unittest.TestCase):
+    def test_squared(self):
+        """
+        """
+        func=lab4.squared
+        case1=[1, 2, 3]
+        expected1=[1, 4, 9]
+        self.assertEqual(func(case1), expected1)
+
+    def test_check_title(self):
+        """
+        """
+        func=lab4.check_title
+        case2=["Hello World", "hi", "Hello"]
+        expected2=["Hello World", "Hello"]
+        self.assertEqual(func(case2), expected2)
+
+    def test_restock_inventory(self):
+        """
+        """
+        func=lab4.restock_inventory
+        case3={'a':10, 'b':5, 'c':7}
+        expected3={'a':20, 'b':15, 'c':17}
+        self.assertEqual(func(case3), expected3)
+
+    def test_filter_0_items(self):
+        """
+        """
+        func=lab4.filter_0_items
+        case4={'a':10, 'b':0, 'c':7}
+        expected4={'a':10, 'c':7}
+        self.assertEqual(func(case4), expected4)
+
+    def test_average_grades(self):
+        """
+        """
+        func=lab4.average_grades
+        case5={'Michael' :[100, 78, 88, 900/10], 'Daniel':[80, 95, 77, 64.0], 'Josh':[99, 89, 94, 66]}
+        expected5={'Michael': 89.0, 'Daniel': 79.0, 'Josh': 87.0}
+        self.assertEqual(func(case5), expected5)
+
+
+
 if __name__ == '__main__':
     try:
         import lab4
